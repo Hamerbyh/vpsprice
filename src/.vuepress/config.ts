b4d68211def9af04d907ecec349f56a9d30a83d9
baoyh <waynebyh@gmail.com>
2024-03-25 10:30:45 +0800

    peizhi

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { sitemapPlugin } from '@vuepress/plugin-sitemap';
 
 export default defineUserConfig({
   base: "/",
@@ -10,6 +11,19 @@ export default defineUserConfig({
 
   theme,
 
+  plugins: [
+    // 调用 sitemapPlugin 并传入配置选项（如果有的话）
+    sitemapPlugin({
+      // 这里是 sitemapPlugin 的配置选项
+      hostname: 'https://vpsprice.net', // 例如，这是生成站点地图时必须的基础 URL
+      // 你可以添加更多选项来定制站点地图的生成
+    }),
+
+    
+  ],
+
   // Enable it with pwa
   // shouldPrefetch: false,
 });
+
+
